ca417e85dd8375230ddd39104bd71514d8f57194
mahek3004 <maheksavaliya3004@gmail.com>
2026-03-25 23:50:14 +0530

    Delete account directory

--- account/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import FitnessProfile
-
-@admin.register(FitnessProfile)
-class FitnessProfileAdmin(admin.ModelAdmin):
-    list_display = ('username', 'email', 'fitness_goal', 'membership_type')
-    list_filter = ('fitness_goal', 'membership_type')
-    search_fields = ('username', 'email')
